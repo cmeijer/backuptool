8796f233d6ee718ba8f59654267fd593eab56aa4
chris <cwmeijer@gmail.com>
2015-07-23 21:13:04 +0200

    main triggering writing functionality

--- src/backuptool/MainClass.java
+++ src/backuptool/MainClass.java
@@ -3,11 +3,13 @@ package backuptool;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-
+		Path path = Paths.get("/home/chris/development/runrunrun");
+		new MainClass().writeFile(path);
 	}
 
 	public void writeFile(Path path) {
