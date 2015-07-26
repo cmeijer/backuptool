d2c0b0e2d96b8838701aabdd7ee16b5f670f895b
chris <cwmeijer@gmail.com>
2015-07-26 19:03:20 +0200

    file copier

--- src/backuptool/FileCopier.java
+++ src/backuptool/FileCopier.java
@@ -1,9 +1,18 @@
 package backuptool;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileCopier {
 
 	public void copy(FileAttributes file, Path sourceDirectory, Path targetDirectory) {
+		try {
+			Path sourcePath = sourceDirectory.resolve(file.getRelativePath());
+			Path targetPath = targetDirectory.resolve(file.getRelativePath());
+			Files.copy(sourcePath, targetPath);
+		} catch (IOException e) {
+			throw new BackupToolException(e);
+		}
 	}
 }
--- src/tests/FileCopierTests.java
+++ src/tests/FileCopierTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -17,13 +18,46 @@ public class FileCopierTests extends BackUpToolTests {
 
 	private Path sourceDirectory = testDirectory.resolve("copyFolder/source");
 	private Path targetDirectory = testDirectory.resolve("copyFolder/target");
+	private String fileName = "file.jpg";
+	private Path expectedTargetFilePath = targetDirectory.resolve(fileName);
+	private Path sourceFilePath = sourceDirectory.resolve(fileName);
 	private FileCopier fileCopier;
 
 	@Test
-	public void copy_emptyFile_fileCreated() {
-		FileAttributes file = new FileAttributes("file.jpg");
+	public void copy_file_fileCreated() {
+		FileAttributes file = new FileAttributes(fileName);
+
+		// Act
 		fileCopier.copy(file, sourceDirectory, targetDirectory);
-		assertTrue(Files.exists(targetDirectory.resolve(file.getRelativePath())));
+
+		// Assert
+		assertTrue(Files.exists(expectedTargetFilePath));
+	}
+
+	@Test
+	public void copy_file_sameSize() throws IOException {
+		FileAttributes file = new FileAttributes(fileName);
+
+		// Act
+		fileCopier.copy(file, sourceDirectory, targetDirectory);
+
+		// Assert
+		assertEquals(Files.size(sourceFilePath), Files.size(expectedTargetFilePath));
+	}
+
+	@Test
+	public void copy_file_sameContent() throws IOException {
+		FileAttributes file = new FileAttributes(fileName);
+
+		// Act
+		fileCopier.copy(file, sourceDirectory, targetDirectory);
+
+		// Assert
+		byte[] sourceBytes = Files.readAllBytes(sourceFilePath);
+		byte[] targetBytes = Files.readAllBytes(expectedTargetFilePath);
+		for (int i = 0; i < sourceBytes.length; i++) {
+			assertEquals(sourceBytes[i], targetBytes[i]);
+		}
 	}
 
 	@Before
@@ -34,6 +68,7 @@ public class FileCopierTests extends BackUpToolTests {
 
 	@After
 	public void cleanUp() throws IOException {
+		Files.delete(expectedTargetFilePath);
 		Files.delete(targetDirectory);
 	}
 
