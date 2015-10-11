6958a674cbd11e6320cfe05f4bf756e4036b2cc9
chris <cwmeijer@gmail.com>
2015-10-11 10:06:48 +0200

    updated scripts

---
+++ copyscript.sh
@@ -0,0 +1,3 @@
+!/bin/bash
+cd /home/chris/development/backuptool
+java -jar target/backuptool-0.0.1-SNAPSHOT-jar-with-dependencies.jar copy
--- script.sh
+++
@@ -1,4 +0,0 @@
-!/bin/bash
-echo "Running script."
-cd /home/chris/development/backuptool
-java -jar target/backuptool-0.0.1-SNAPSHOT.jar
